6a988077d84d97753e13a36caaa2c5d6db5aff9d
Matilda Mared <matildamared@live.se>
2022-08-21 13:18:12 +0200

    add basic move functionality

--- code/player.py
+++ code/player.py
@@ -1,25 +1,44 @@
 import pygame
 from settings import *
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        # General setup
         self.image = pygame.Surface((32, 64))
         self.image.fill("pink")
         self.rect = self.image.get_rect(center=pos)
 
+        # Movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # Horizontal movement
         if keys[pygame.K_UP]:
-            print("up")
+            self.direction.y = -1
         elif keys[pygame.K_DOWN]:
-            print("down")
-        elif keys[pygame.K_RIGHT]:
-            print("right")
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        # Vertical movement
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
         elif keys[pygame.K_LEFT]:
-            print("left")
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
 
     def update(self, dt):
         self.input()
+        self.move(dt)
